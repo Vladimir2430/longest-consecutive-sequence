46b1b48e56e8a5a04494a458aeb8be53bc1bb142
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2018-07-23 21:36:38 +0300

    code improved

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 module.exports = function longestConsecutiveLength(array) {
-  array.sort(function(a, b) {
-    return a - b;
-  });
-  var count=0;
-  var sum=1;
-  for (let i=0; i<array.length; i++) {
-    if (array[i]==array[i+1]) {continue;}
-    if (array[i]+1==array[i+1]) {sum+=1;
+  let count = 0, sum = 1;
+  array.sort((a, b) => a - b);
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === array[i + 1]) {continue;}
+    if (array[i] + 1 === array[i + 1]) {sum += 1;
     } else {
-      if (sum>count) {count=sum;}
-      sum=1;
+      if (sum > count) {count = sum;}
+      sum = 1;
     }
   }
   return count;
